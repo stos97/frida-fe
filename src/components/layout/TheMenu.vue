562f5b6da8f399f1371e573efaa7f4f0beccabf8
tos <stevann.tosic@gmail.com>
2025-02-07 11:13:43 +0100

    convert menu to composition api

--- src/components/layout/TheMenu.vue
+++ src/components/layout/TheMenu.vue
@@ -7,7 +7,7 @@
     </ion-header>
     <ion-content>
       <ion-list>
-        <ion-item v-for="menuItem in menuItems" @click="navigateTo(menuItem.redirectTo)" :key="menuItem.redirectTo">{{menuItem.menuName}}</ion-item>
+        <ion-item v-for="menuItem in menuItems" @click="navigateTo(menuItem.redirectTo)" :key="menuItem.redirectTo">{{ menuItem.menuName }}</ion-item>
         <ion-item @click="logout">Odjavi se</ion-item>
       </ion-list>
     </ion-content>
@@ -15,71 +15,56 @@
 </template>
 
 <script>
-import {IonContent, IonHeader, IonTitle, IonToolbar, menuController, IonItem, IonList, IonMenu} from "@ionic/vue";
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import { menuController } from '@ionic/vue';
 
 export default {
-  components: {
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar,
-    IonItem,
-    IonList,
-    IonMenu,
-  },
-  data() {
-    return {
-      menuItems: []
-    };
-  },
-  created() {
-    this.menuItems = this.getMenuOptions()
-  },
-  methods: {
-    async navigateTo(to) {
-      await menuController.close('menu');
-      this.$router.push(to);
-    },
-    async logout() {
-      try {
-        await this.$store.dispatch('logout');
-        await menuController.close('menu');
-        window.location.assign('/');
-      } catch (err) {
-        console.log(err)
-      }
-    },
-    getMenuOptions() {
-      const role = this.$store.getters.user.role;
+  setup() {
+    const menuItems = ref([]);
+    const router = useRouter();
+    const store = useStore();
+
+    const getMenuOptions = () => {
+      const role = store.getters.user.role;
 
       if (role === 'admin') {
         return [
-          {
-            redirectTo: '/admin',
-            menuName: 'Pocetna'
-          },
-          {
-            redirectTo: '/categories',
-            menuName: 'Kategorije'
-          },
-          {
-            redirectTo: '/services',
-            menuName: 'Usluge'
-          },
-          {
-            redirectTo: '/additions',
-            menuName: 'Dodaci'
-          },
+          { redirectTo: '/admin', menuName: 'Pocetna' },
+          { redirectTo: '/categories', menuName: 'Kategorije' },
+          { redirectTo: '/services', menuName: 'Usluge' },
+          { redirectTo: '/additions', menuName: 'Dodaci' },
         ];
       } else {
-        return [
-          {
-            redirectTo: '/user',
-            menuName: 'Users'
-          }
-        ]
+        return [{ redirectTo: '/user', menuName: 'Users' }];
       }
-    }
-  }
-}
+    };
+
+    const navigateTo = async (to) => {
+      await menuController.close('menu');
+      await router.push(to);
+    };
+
+    const logout = async () => {
+      try {
+        await store.dispatch('logout');
+        await menuController.close('menu');
+        window.location.assign('/');
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    onMounted(() => {
+      menuItems.value = getMenuOptions();
+    });
+
+    return {
+      menuItems,
+      navigateTo,
+      logout,
+    };
+  },
+};
 </script>
